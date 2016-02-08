e51c8359218a6d76f98bbea47d1e012af0d07d35
TarekkMA <tarekkma@gmail.com>
2016-02-08 16:12:58 +0200

    added v1 to git

---
+++ app/src/main/java/com/TMAProject/root/tasks/rootcalculator/MainActivity.java
@@ -0,0 +1,52 @@
+package com.TMAProject.root.tasks.rootcalculator;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        EditText equationText = (EditText)findViewById(R.id.editText);
+        final TextView resText     = (TextView)findViewById(R.id.resText);
+        equationText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                try{
+                    Expression exp = new ExpressionBuilder(s.toString()).build();
+                    resText.setText(String.valueOf(exp.evaluate()));
+                }catch (Exception e){
+                    resText.setText("Math Error!");
+                }
+            }
+        });
+    }
+}
